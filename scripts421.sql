6adbced1439ae1b1b239ac9ac1911d62b57ddbc2
axel <lehander2016@ya.ru>
2023-05-08 15:19:43 +0300

    homework done

--- scripts421.sql
+++ scripts421.sql
@@ -2,4 +2,6 @@ ALTER TABLE student ADD CONSTRAINT age_constraint CHECK ( age > 16 );
 
 ALTER TABLE student ADD CONSTRAINT name_unique UNIQUE (name), ALTER COLUMN name SET NOT NULL;
 
+ALTER TABLE faculty ADD CONSTRAINT color_unique UNIQUE (color);
+
 ALTER TABLE student ALTER COLUMN age SET DEFAULT 20;
